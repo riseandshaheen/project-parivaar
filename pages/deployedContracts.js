ea240fa51e7a25e0996249024487c3cd9dfd3737
Shaheen <91434451+riseandshaheen@users.noreply.github.com>
2022-02-22 19:11:04 +0000

    Env variable added

--- pages/deployedContracts.js
+++ pages/deployedContracts.js
@@ -9,7 +9,7 @@ export default function DeployedContracts(){
   const useraddr = accountss[0];
 
   
-  const url = `https://api-rinkeby.etherscan.io/api?module=account&action=balance&address=${useraddr}&tag=latest&apikey=9IIYAP1RY7KHZ3DDSBAJ6GNB1ITZ4K1XSP`
+  const url = `https://api-rinkeby.etherscan.io/api?module=account&action=balance&address=${useraddr}&tag=latest&apikey=${process.env['API_KEY']}`
 
   const response = fetch(url);
   const data = response.json();
